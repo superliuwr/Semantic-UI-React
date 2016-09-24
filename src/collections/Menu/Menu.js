430a4d28a2f1da9b83ad12d0b14c96026f6557fe
Jeff Carbonella <jeff@contactually.com>
2016-09-24 00:57:19 -0400

    feat(Menu): Fix issues with items shorthand
    - Use the createShorthand factory to create a MenuItem for each item.
    - Do not infer activeIndex from the items array on mount.
    
    Regarding the second point, there were a few issues with it:
    - The array items may not be objects - they could be primitives or elements
    - You should be able to reliably set the activeIndex or defaultActiveIndex
    via props, but this would overwrite that.

--- src/collections/Menu/Menu.js
+++ src/collections/Menu/Menu.js
@@ -15,6 +15,7 @@ import {
   useValueAndKey,
   useWidthProp,
 } from '../../lib'
+import { createShorthand } from '../../factories'
 import MenuHeader from './MenuHeader'
 import MenuItem from './MenuItem'
 import MenuMenu from './MenuMenu'
